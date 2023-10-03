cd4dadbbe80dcd4931a8075753a7170bcd303ae0
Joel Yvan Heda Tankeu <inf4055@hs-worms.de>
2023-10-03 14:02:21 +0200

    configure a search component

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
 
+  constructor(){}
+
+  ngOnInit(): void {
+      
+  }
+
+  onSubmit(form: NgForm){
+    console.log(form.value)
+  }
 }
---
+++ src/app/shared/music.ts
@@ -0,0 +1,9 @@
+export class Music{
+    constructor(
+        private artistName: String,
+        private artistViewUrl: String,
+        private artworkUrl30: String,
+        private artworkUrl60: String,
+        private artworkUrl100: String,
+        private trackId: Number){}
+}
